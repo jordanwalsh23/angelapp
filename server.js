872537933e665d0163b0f8099077d4456915a6fe
Jordan Walsh <jordanwalsh23@hotmail.com>
2016-03-28 20:48:51 +1100

    added renew method

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ apiRoutes.post('/authenticate', function(req, res) {
         // if user is found and password is right
         // create a token
         var token = jwt.sign(user, app.get('superSecret'), {
-          expiresInMinutes: 1440 // expires in 24 hours
+          expiresIn: 20 // expires in 24 hours (86400s)
         });
 
         // return the information including token as JSON
@@ -72,6 +72,63 @@ apiRoutes.post('/authenticate', function(req, res) {
       }
     }
   });
+});
+
+// route to renew and expired token (POST http://localhost:8080/api/renew)
+apiRoutes.post('/renew', function(req, res) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), {
+      ignoreExpiration: true
+    }, function(err, decoded) {
+      if (err) {
+        return res.status(403).send({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+
+        var id = decoded && decoded._doc && decoded._doc._id ? decoded._doc._id : false;
+
+        if(id) {
+          console.log("renewing token for "+id);
+
+          User.findOne({
+            _id: id
+          }, function(err, user) {
+            if(err || user == null) {
+              return res.status(403).send({ success: false, message: 'Failed to authenticate token. (User error)' });
+            } else {
+              var token = jwt.sign(user, app.get('superSecret'), {
+                expiresIn: 20 // expires in 24 hours
+              });
+
+              // return the information including token as JSON
+              res.json({
+                success: true,
+                message: 'Enjoy your token!',
+                token: token
+              });
+            }
+          });
+        } else {
+          return res.status(403).send({ success: false, message: 'Failed to authenticate token. (User error)' });
+        }
+      }
+    });
+
+  } else {
+    // if there is no token
+    // return an error
+    return res.status(422).send({
+        success: false,
+        message: 'No token provided.'
+    });
+  }
+
+
 });
 
 // route to authenticate a user (POST http://localhost:8080/api/authenticate)
